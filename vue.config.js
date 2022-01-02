06122b1f3eb5c353ce516de5f24badaf1fa0ce39
unknown <abanoubsamir@gmail.com>
2022-01-02 21:42:06 +0200

    modify

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,13 @@ module.exports = {
   ],
   // publicPath: process.env.NODE_ENV === "production" ? "http://localhost/music%20project/dist/" : "/",
   publicPath: process.env.NODE_ENV === "production" ? "/ArtistAbanoubSamir" : "/",
+
+  chainWebpack: config => {
+    config
+        .plugin('html')
+        .tap(args => {
+            args[0].title = "Abanoub Samir Music Page";
+            return args;
+        })
+}
 }
