17c22ba94e3685dea3942752b28a6c8f2b6f1759
abanoubsamirnaguib <abanoubsamir@gmail.com>
2022-02-23 15:01:42 +0200

    cap

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,31 @@ module.exports = {
             args[0].title = "Abanoub Samir Music Page";
             return args;
         })
+},
+
+
+// sitemap
+configureWebpack: {
+  plugins: [
+      new SitemapPlugin('https://abanoubsamirnaguib.github.io/ArtistAbanoubSamir/', paths, {
+          filename: 'sitemap.xml',
+          lastmod: true,
+          changefreq: 'hourly',
+          priority: '0.8'
+      })
+  ]
 }
+
 }
+
+
+// sitemap
+const SitemapPlugin = require('sitemap-webpack-plugin').default;
+const paths = [
+    {
+        path: '/',
+        lastmod: new Date().toISOString().slice(0,10),
+        priority: '0.8',
+        changefreq: 'hourly'
+    },
+];
